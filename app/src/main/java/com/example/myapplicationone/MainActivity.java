27b75057098d59e28df9b14082d6f212b80e7b46
githubuseraryan <deshmukhyaaryan@gmail.com>
2021-02-09 20:17:52 +0530

    first commit

---
+++ app/src/main/java/com/example/myapplicationone/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.myapplicationone;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MainActivity extends AppCompatActivity {
+    private ToggleButton TglbtnFlsh;
+    private CameraManager CmraMngr;
+    private String CmraId;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!isFlashAvailable)
+            noFlashAvailableError();
+
+        CmraMngr = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            CmraId = CmraMngr.getCameraIdList()[0];
+        } catch (CameraAccessException q) {
+            q.printStackTrace();
+        }
+
+
+        ToggleButton TglbtnFlsh = findViewById(R.id.Button2);
+        TglbtnFlsh.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    CmraMngr.setTorchMode(CmraId, isChecked);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        TglbtnFlsh.setOnClickListener(new CompoundButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+
+                    case Configuration.UI_MODE_NIGHT_YES:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+
+                    case Configuration.UI_MODE_NIGHT_NO:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
+            }
+        });
+    }
+
+    private void noFlashAvailableError() {
+        Toast.makeText(MainActivity.this, "Torch not Available", Toast.LENGTH_SHORT).show();
+    }
+}
